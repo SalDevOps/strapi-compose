bac33adad5958a74079b22fa3fde17a379ff6b7f
Hector Salvador [SalDevOps] <hector.salvador@live.com>
2022-02-06 16:09:25 -0300

    Improved handling of env variables replacement in nginx conf All backend base-urls to be read from a config file

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+
 PUBLIC_IP=$(curl -s ifconfig.me)
 echo "Starting nginx in $PUBLIC_IP"
-LOG_REWRITES="${LOG_REWRITES:-0}" \
-LOG_HEALTH_CHECKS="${LOG_HEALTH_CHECKS:-0}" \
-FRONTEND_HOST="${FRONTEND_HOST:-localhost}" \
-BACKEND_HOST="${FRONTEND_HOST:-$FRONTEND_HOST}" \
-envsubst '$API_SERVER_ADDR $API_SERVER_PORT $LOG_REWRITES $LOG_HEALTH_CHECKS $FRONTEND_HOST $BACKEND_HOST' \
-        < /tmp/template.conf \
-        > /etc/nginx/conf.d/default.conf \
-    && echo "Starting nginx engine with following config" \
-    && cat /etc/nginx/conf.d/default.conf \
-    && nginx -g 'daemon off;'
+
+# To be used to isolate the /admin routes:
+export  NG_FRONTEND_DOMAIN="${NG_FRONTEND_DOMAIN:-localhost}" \
+        NG_BACKEND_DOMAIN="${NG_FRONTEND_DOMAIN:-$NG_FRONTEND_DOMAIN}" \
+        NG_LOG_REWRITES="${NG_LOG_REWRITES:-0}" \
+        NG_LOG_HEALTH_CHECKS="${NG_LOG_HEALTH_CHECKS:-0}"
+
+# All Strapi Admin related URLs (as per in upstream.ini file)
+export  NG_BACKEND_URLS='('"$(grep -vE '^[ #]+|^$' upstream.ini | xargs | sed -e 's/ /|/g')"')'
+echo "Found Backend URLs ${NG_BACKEND_URLS}"
+
+# Only Environment variables starting with API_ or NG_ will be exported/replaced in nginx config file:
+export  NGINX_ENV="$(compgen -A variable | grep -E '(API|NG)_' | sed -e 's/^/$/' | xargs | sed -e 's/ / /g')"
+echo "Replacing Environment Variables ${NGINX_ENV}"
+envsubst "$NGINX_ENV" \
+    < /tmp/template.conf \
+    > /etc/nginx/conf.d/default.conf
+# cat /etc/nginx/conf.d/default.conf
+
+echo "✅ NGINX Engines Ready"
+nginx -g 'daemon off;'
