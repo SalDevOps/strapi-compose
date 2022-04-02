47a8a8e4461d9e36a6f7334f8fb34078c68e6ed2
Hector Salvador [SalDevOps] <52145773+SalvadorCodes@users.noreply.github.com>
2022-04-02 16:11:39 -0300

    Service to wrap any static FrondEnd app (react/angular/etc) to be properly served as static files by NGIИX

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+ENV_VARS_PREFIX=${ENV_VARS_PREFIX:-"NG_"}
+CONFIG_TEMPLATE=${CONFIG_TEMPLATE:-/tmp/template.conf}
+
+prepare_config() {
+    # Only Environment variables starting with $ENV_VARS_PREFIX will be exported/replaced in nginx config file:
+    export  NGINX_ENV="$(compgen -A variable | grep -E "${ENV_VARS_PREFIX}" | sed -e 's/^/$/' | xargs | sed -e 's/ / /g')"
+    echo "Replacing Environment Variables ${NGINX_ENV}"
+    envsubst "$NGINX_ENV" \
+        < ${CONFIG_TEMPLATE} \
+        > /etc/nginx/conf.d/default.conf
+}
+
+start_nginx() {
+    prepare_config
+    echo "✅ Starting to Server FrontEnd app with NGINX..."
+    nginx -g 'daemon off;'
+}
+
+# Start NginX by default (if no command has been requested)
+# otherwise, try directly running the requested command/parameters
+[[ -z "$1" ]] && start_nginx || exec "$@"
